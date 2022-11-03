a995f92602132a247fcd9426a9b2b71d8d5d24fa
rafapereirabr <rafa.pereira.br@gmail.com>
2022-11-03 15:07:34 -0300

    download and prep munic data

--- R/01_data_download.R
+++ R/01_data_download.R
@@ -1,5 +1,9 @@
 # download and unzip data
 
+library(openxlsx)
+library(data.table)
+
+
 
 #### eleitorado --------------------------------------------------
 #' perfil de escolaridade em cada zona
@@ -76,3 +80,41 @@ download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/detalhe_votacao_se
 # unzip data to local dir
 unzip(paste0(dir_secoes, "/secoes_2022.zip"), exdir = dir_secoes)
 unzip(paste0(dir_secoes, "/secoes_2018.zip"), exdir = dir_secoes)
+
+
+
+
+#### MUNIC data --------------------------------------------------
+#' https://www.ibge.gov.br/estatisticas/sociais/saude/10586-pesquisa-de-informacoes-basicas-municipais.html?=&t=downloads
+
+# create dir
+dir_munic_2020 <- './data_raw/munic'
+dir.create(dir_munic_2020)
+
+# download data
+download.file("https://ftp.ibge.gov.br/Perfil_Municipios/2020/Base_de_Dados/Base_MUNIC_2020.xlsx",
+              destfile = paste0(dir_munic_2020,"/Base_MUNIC_2020.xlsx"))
+
+
+# read data
+munic <- openxlsx::read.xlsx(xlsxFile = './data_raw/munic/Base_MUNIC_2020.xlsx',
+                         sheet = 'Transporte')
+
+
+# Fix Municipio of Ananindeua did not answer the survey, but they do have public transport
+setDT(munic)[, Mtra19 := fifelse(CodMun == 1500800, 'Sim', Mtra19)]
+
+
+table(munic$Mtra19)
+#>  Não Não informou       Recusa          Sim 
+#> 3740           13           89         1728 
+
+# create dummy
+munic[, dummy_pt := fifelse(Mtra19 == 'Sim', 1, 0)]
+
+# subset and rename columns
+munic <- munic[, .(CodMun, UF, Cod.UF, Mun, dummy_pt)]
+setnames(munic, c('code_muni', 'UF', 'code_state', 'name_muni', 'dummy_pt'))
+
+# save
+fwrite(munic, './data/munic_dummy_pt.csv')
