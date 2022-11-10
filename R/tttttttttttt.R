7afd9050edbbcb731fb71055c4a46b4d908261ec
rafapereirabr <rafa.pereira.br@gmail.com>
2022-11-10 16:20:48 -0300

    uptade reg

--- R/tttttttttttt.R
+++ R/tttttttttttt.R
@@ -22,22 +22,19 @@ library(ggplot2)
 options(scipen = 999)
 
 # ler base de eleições
-BD <- fread("./data/base_DiD2022_secoes.csv")
+BD <- fread("../../data/base_DiD2022_secoes.csv")
 
 
-# pib <- fread("../../data_raw/IBGE/PIBPC_2019_municipios.csv", encoding = "UTF-8")
-# BD <- merge(BD, pib, by="code_muni")
-# summary(BD$PIB_PC)
-
+# convert selected columns to log
 BD[, pib_log := log(PIB_PC)]
-
+BD[, QT_APTOS_log := log(QT_APTOS)]
 
 # excluir seções de cidades sem sistema de ônibus
 BD <- subset(BD, dummy_pt==1)
 BD[, table(NR_TURNO, passe_livre)]
 
 
-# drop always treated
+# identify treated in the 1st round
 BD[, passe_livre_1 := max(NR_TURNO==1 & passe_livre==1), by = id_secao]
 table(BD$passe_livre_1)
 
@@ -46,6 +43,7 @@ BD[, table(NR_TURNO, passe_livre_1)]
 
 BD[NR_TURNO==2, table(passe_livre_1, passe_livre)]
 
+# drop always treated
 df2 <- BD[ passe_livre_1 != 1]
 df2[, table(NR_TURNO, passe_livre_1)]
 
@@ -78,13 +76,14 @@ summary(dd1)
 
 # share bolso votes
 df_muni <- df2[NR_TURNO==2, .(QT_APTOS = sum(QT_APTOS),
+                              # educacao_1 = mean(educacao_1),
                               gov_2t = max(gov_2t),
                               pib_log = pib_log[1L],
                               passe_livre_2 = max(passe_livre_2)), 
                by=code_muni]
 
 
-step1 <- glm(passe_livre_2 ~ gov_2t + log(QT_APTOS) + pib_log, 
+step1 <- glm(passe_livre_2 ~ gov_2t + log(QT_APTOS)  + pib_log, 
              family = binomial(link = 'logit'),
              data = df_muni)
 
@@ -155,7 +154,7 @@ get_reg_group <- function(l, u){  # l = 0; u = .1
  temp_df <- df2[ educacao_1 >= l & educacao_1 <= u]
 
  
- step2 <- fixest::feols(comparecimento_2022~turno2_dummy + passe_livre_2 + turno2_dummy:passe_livre_2, 
+ step2 <- fixest::feols(comparecimento_2022~turno2_dummy + closest_dist + passe_livre_2 + turno2_dummy:passe_livre_2, 
                         fixef = 'id_secao', 
                         cluster = 'code_muni',
                         weights = ~ipw,
