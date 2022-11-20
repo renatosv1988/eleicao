4263a6c2066a0f78e1777de045c6460b55b6bf41
rafapereirabr <rafa.pereira.br@gmail.com>
2022-11-19 21:16:02 -0300

    update clean passe livre

--- R/02_clean_passe_livre.R
+++ R/02_clean_passe_livre.R
@@ -2,12 +2,36 @@ library(readxl)
 library(data.table)
 library(stringr)
 
-# read passe livre
-passe_livre <- read_excel("../../data_raw/passe_livre/Passe Livre nas Eleições.xlsx", range = "B3:N396")
+`%nin%` <- negate(`%in%`)
 
-# read zonas 
-files_zonas <- list.files(path = '../../data_raw/zonas', pattern = '.csv', full.names = T)
-zonas <- fread( files_zonas[2], nrows = Inf)
+
+# read passe livre
+passe_livre <- read_excel("../../data_raw/passe_livre/Passe Livre nas Eleições.xlsx", range = "B3:N397")
+setDT(passe_livre)
+
+# municipios with passe livre all year
+passe_livre_sempre <- read_excel("../../data_raw/passe_livre/Passe Livre nas Eleições.xlsx", range = "B427:C480")
+setDT(passe_livre_sempre)
+setnames(passe_livre_sempre, 'Estado', 'UF')
+
+
+
+
+### read zonas 
+
+ # unzip data
+ temp_dir_1 <- tempdir()
+ all_zip_files <- list.files("../../data_raw/zonas", full.names = T, pattern = '.zip')
+ temp_zip <- all_zip_files[all_zip_files %like% 2022]
+ files <- unzip(temp_zip, list=T)$Name
+ path_csv_T1  <- files[files %like% '.csv']
+ unzip(temp_zip, files  = path_csv_T1, exdir = temp_dir_1)
+ path_csv <- paste0(temp_dir_1,'/',path_csv_T1)
+ 
+ # read zonas
+ zonas <- fread( path_csv)
+
+
 
 # create data frame of municipalities with codes
 municipios <- zonas[!duplicated(zonas$CD_MUNICIPIO,),
@@ -16,59 +40,123 @@ municipios <- zonas[!duplicated(zonas$CD_MUNICIPIO,),
 municipios <- municipios[municipios$SG_UF!="ZZ",]
 
 # create city - UF ID
-municipios$city_uf_ID <- paste(municipios$NM_MUNICIPIO, municipios$SG_UF, sep =" - ")
-passe_livre$city_uf_ID <- paste(passe_livre$Cidade, passe_livre$UF, sep =" - ")
+municipios[, city_uf_ID := paste( NM_MUNICIPIO, SG_UF, sep =" - ")]
+passe_livre[, city_uf_ID := paste( Cidade, UF, sep =" - ")]
+passe_livre_sempre[, city_uf_ID := paste( Cidade, UF, sep =" - ")]
+
 
 # clean special characters and make all upper case
-municipios$city_uf_ID <- iconv(municipios$city_uf_ID, to = "ASCII//TRANSLIT")
-municipios$city_uf_ID <- toupper(municipios$city_uf_ID)
-passe_livre$city_uf_ID <- iconv(passe_livre$city_uf_ID, from = "UTF-8", to = "ASCII//TRANSLIT")
-passe_livre$city_uf_ID <- toupper(passe_livre$city_uf_ID)
+municipios[, city_uf_ID := iconv(city_uf_ID, to = "ASCII//TRANSLIT")]
+municipios[, city_uf_ID := toupper(city_uf_ID)]
+passe_livre[, city_uf_ID := iconv(city_uf_ID, from = "UTF-8", to = "ASCII//TRANSLIT")]
+passe_livre[, city_uf_ID := toupper(city_uf_ID)]
+passe_livre_sempre[, city_uf_ID := iconv(city_uf_ID, from = "UTF-8", to = "ASCII//TRANSLIT")]
+passe_livre_sempre[, city_uf_ID := toupper(city_uf_ID)]
+
+
+
+
 
 ## ADDITIONAL MANUAL FIXES
 # remove double spaces
-passe_livre$city_uf_ID <- str_squish(passe_livre$city_uf_ID)
+passe_livre[, city_uf_ID := str_squish(city_uf_ID)]
+passe_livre_sempre[, city_uf_ID := str_squish(city_uf_ID)]
 
 # typos
-passe_livre$city_uf_ID <- gsub("SANTO ANTONIO DA POSSE","SANTO ANTONIO DE POSSE",passe_livre$city_uf_ID)
-passe_livre$city_uf_ID <- gsub("SAO LUIZ DO PARAITINGA","SAO LUIS DO PARAITINGA",passe_livre$city_uf_ID)
+passe_livre[, city_uf_ID := gsub("SANTO ANTONIO DA POSSE","SANTO ANTONIO DE POSSE", city_uf_ID)]
+passe_livre[, city_uf_ID := gsub("SAO LUIZ DO PARAITINGA","SAO LUIS DO PARAITINGA", city_uf_ID)]
+passe_livre_sempre[, city_uf_ID := gsub("SANTO ANTONIO DA POSSE","SANTO ANTONIO DE POSSE", city_uf_ID)]
+passe_livre_sempre[, city_uf_ID := gsub("SAO LUIZ DO PARAITINGA","SAO LUIS DO PARAITINGA", city_uf_ID)]
+
+
+# Update of munis with passe livre all year
+passe_livre <- rbindlist(fill = T,
+                         list(passe_livre,  # avoid repetition
+                              subset(passe_livre_sempre, city_uf_ID %nin% passe_livre$city_uf_ID)
+                         )
+)
+
+# rename columns
+setnames(passe_livre, '1º Turno', 'passe_livre_t1')
+setnames(passe_livre, '2º Turno', 'passe_livre_t2')
+
+
+# update passe livre info
+passe_livre[ city_uf_ID %in% passe_livre_sempre$city_uf_ID, passe_livre_t1 := 'S' ]
+passe_livre[ city_uf_ID %in% passe_livre_sempre$city_uf_ID, passe_livre_t2 := 'S' ]
+
+sum(passe_livre$passe_livre_t1 == 'S' , na.rm=T)
+#> 135
+
+sum(passe_livre$passe_livre_t2 == 'S' , na.rm=T)
+#> 432
+
+# MAJOR DISCOUNT ???? ON 1ST ROUND
+passe_livre[ city_uf_ID %like% 'RIO BRANCO']
+passe_livre[ city_uf_ID %like% 'NATAL']
+passe_livre[ city_uf_ID %like% 'CUIABA']
+passe_livre[ city_uf_ID %like% 'JUNDIAI']
+
+passe_livre[ city_uf_ID %like% 'AMERICANA'] # passe metropolitano mas nao muni
 
 # merge CD_MUNICIPIO to passe_livre
 passe_livre <- merge(passe_livre, municipios[,c("city_uf_ID", "CD_MUNICIPIO")], by= "city_uf_ID", all.x = T)
 
-# convert S/N to 1/0
-passe_livre$`1º Turno`[passe_livre$`1º Turno`%in%"S"] <- 1
-passe_livre$`1º Turno`[passe_livre$`1º Turno`%in%"N"] <- 0
-passe_livre$`2º Turno`[passe_livre$`2º Turno`%in%"S"] <- 1
-passe_livre$`2º Turno`[passe_livre$`2º Turno`%in%"N"] <- 0
 
-# convert NAs to 0
-passe_livre$`1º Turno`[is.na(passe_livre$`1º Turno`)] <- 0
-passe_livre$`2º Turno`[is.na(passe_livre$`2º Turno`)] <- 0
 
-# remove SUMARÉ (NA both rounds)
-passe_livre <- passe_livre[!passe_livre$city_uf_ID=="SUMARE - SP",]
+passe_livre[, passe_livre_t1 := fcase(passe_livre_t1=='S',1,
+                                      passe_livre_t1=='N', 0,
+                                      default = 0)]
 
-# considerar municípios que tem passe livre sempre
-sempre_livre <- read_excel("../../data_raw/passe_livre/Passe Livre nas Eleições.xlsx", range = "B427:C480")
-sempre_livre$city_uf_ID <- paste(sempre_livre$Cidade, sempre_livre$Estado, sep =" - ")
-sempre_livre$city_uf_ID <- iconv(sempre_livre$city_uf_ID, from = "UTF-8", to = "ASCII//TRANSLIT")
-sempre_livre$city_uf_ID <- toupper(sempre_livre$city_uf_ID)
-sempre_livre$city_uf_ID <- gsub("ARTHUR NOGUEIRA - SP","ARTUR NOGUEIRA - SP",sempre_livre$city_uf_ID)
-sempre_livre <- merge(sempre_livre, municipios[,c("city_uf_ID", "CD_MUNICIPIO")], by= "city_uf_ID", all.x = T)
-municipios_sempre_livre <- sempre_livre$CD_MUNICIPIO
-passe_livre$`1º Turno`[passe_livre$CD_MUNICIPIO%in%municipios_sempre_livre] <- 1
-passe_livre$`2º Turno`[passe_livre$CD_MUNICIPIO%in%municipios_sempre_livre] <- 1
+passe_livre[, passe_livre_t2 := fcase(passe_livre_t2=='S',1,
+                                      passe_livre_t2=='N', 0,
+                                      default = 0)]
 
+
+# SUMARÉ teve no 2o turno
+passe_livre[city_uf_ID =="SUMARE - SP", passe_livre_t2 := 1]
+#' https://g1.globo.com/sp/campinas-regiao/eleicoes/2022/noticia/2022/10/29/eleicoes-18-cidades-da-regiao-de-campinas-terao-gratuidade-no-transporte-no-2o-turno-veja-quais.ghtml
 # criar versão simplificada do arquivo
-passe_livre_resumo <- passe_livre[,c("CD_MUNICIPIO", "1º Turno","2º Turno")]
-colnames(passe_livre_resumo) <- c("CD_MUNICIPIO", "passe_livre_t1","passe_livre_t2")
 
-sum(as.numeric(passe_livre_resumo$passe_livre_t1))
-sum(as.numeric(passe_livre_resumo$passe_livre_t2))
+# identify cities with permanent passe livre
+passe_livre[city_uf_ID %in% passe_livre_sempre$city_uf_ID, passe_livre_always := 1]
+
+table(passe_livre$passe_livre_t1, useNA = 'always')
+#>    0    1 <NA> 
+#>  298  135    0 
+
+table(passe_livre$passe_livre_t2, useNA = 'always')
+#>  0    1 <NA> 
+#>  1  433    0
+
+
+table(passe_livre$passe_livre_always, useNA = 'always')
+#>   1 <NA> 
+#>  53  380 
+
+
+
+# subset of columns
+passe_livre_resumo <- passe_livre[,c("CD_MUNICIPIO", "passe_livre_t1","passe_livre_t2", "passe_livre_always")]
 
 # save
 dir_passe_livre <- '../../data/passe_livre'
 dir.create(dir_passe_livre)
-write.csv(passe_livre, paste0(dir_passe_livre,"/passe_livre.csv"), row.names = F)
-write.csv(passe_livre_resumo, paste0(dir_passe_livre,"/passe_livre_resumo.csv"), row.names = F)
+fwrite(passe_livre, paste0(dir_passe_livre,"/passe_livre.csv"), row.names = F)
+fwrite(passe_livre_resumo, paste0(dir_passe_livre,"/passe_livre_resumo.csv"), row.names = F)
+
+
+# https://g1.globo.com/sp/campinas-regiao/eleicoes/2022/noticia/2022/10/29/eleicoes-18-cidades-da-regiao-de-campinas-terao-gratuidade-no-transporte-no-2o-turno-veja-quais.ghtml
+# 
+# ano todo
+# Holambra
+# Louveira (ano todo, mas idec fala soh 2)
+# Morungaba
+# 
+passe_livre[ city_uf_ID %like% 'LOUVEIRA']
+# 
+# Americana
+# Jaguariúna (intermunicipal)
+# Paulínia (intermunicipal)
+# Vinhedo
+
