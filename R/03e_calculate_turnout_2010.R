9efbdae586b20e327e33cb4d72a27192b5c265c3
Renato Vieira <renato.sv.1988@gmail.com>
2022-11-26 11:24:01 -0300

    added 2010

---
+++ R/03e_calculate_turnout_2010.R
@@ -0,0 +1,53 @@
+library(data.table)
+library(dplyr)
+
+
+#### secoes --------------------------------------------------
+#' quantidade de votos nominais e validos
+
+# unzip data
+temp_dir_1 <- tempdir()
+all_zip_files <- list.files("../../data_raw/secoes", full.names = T, pattern = '.zip')
+temp_zip <- all_zip_files[all_zip_files %like% 2010]
+files <- unzip(temp_zip, list=T)$Name
+path_csv_T1  <- files[files %like% '.csv']
+path_csv_T1 <- path_csv_T1[path_csv_T1 %like% '2010_BRASIL.csv']
+unzip(temp_zip, files  = path_csv_T1, exdir = temp_dir_1)
+path_csv <- paste0(temp_dir_1,'/',path_csv_T1)
+
+
+# read
+secao <- fread(path_csv,
+               # nrows = Inf, 
+               encoding = "Latin-1")
+
+# remove temp files
+unlink(temp_dir_1, recursive = T)
+rm(list=setdiff(ls(), "secao"))
+gc()
+
+
+# manter apenas votos para presidente no BR
+secao_br <- subset(secao, DS_CARGO == 'Presidente' & SG_UF != 'ZZ')
+
+# calcular comparecimento e abstencao
+secao_br[, comparecimento_2014 := QT_COMPARECIMENTO / QT_APTOS]
+secao_br[, abstencao_2014 := QT_ABSTENCOES / QT_APTOS]
+
+# incluir 2o turno governador
+gov <- subset(secao, DS_CARGO == 'Governador' & SG_UF != 'ZZ' & NR_TURNO==2)
+gov$gov_2t <- 1
+gov <- gov[, .(gov_2t = mean(gov_2t)), by = SG_UF]
+secao_br <- merge(secao_br, gov, by ="SG_UF", all.x = T)
+secao_br$gov_2t[is.na(secao_br$gov_2t)] <- 0
+secao_br$gov_2t <- ifelse(secao_br$NR_TURNO==2, secao_br$gov_2t, 0)
+
+
+
+
+#### save outputs --------------------------------------------------
+dir.create(path = '../../data/secoes')
+
+# comparecimento municipios
+fwrite(secao_br, '../../data/secoes/secoes_2010.csv')
+
